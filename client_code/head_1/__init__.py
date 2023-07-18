5d2169c19ffc2c2e0774e984f638d6b68c9e1997
info@maceam.com <info@maceam.com>
2023-07-18 06:20:05 +0000

    Created Main, Contact and About; Edited settings and head_1; Deleted Home

--- client_code/head_1/__init__.py
+++ client_code/head_1/__init__.py
@@ -9,9 +9,7 @@ class head_1(head_1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
-
   def home_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
@@ -27,4 +25,8 @@ class head_1(head_1Template):
   def contact_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
+
+  def Main_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    
 
