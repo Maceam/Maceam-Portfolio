87afa5ff6a9bf54f61dc81e01a9cc80c09a883b0
info@maceam.com <info@maceam.com>
2023-07-18 05:05:47 +0000

    Created head_1; Edited theme.css, settings, standard-page.html and Home; Deleted Header and Header.ItemTemplate1

--- client_code/head_1/__init__.py
+++ client_code/head_1/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import HeaderTemplate
+from ._anvil_designer import head_1Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Header(HeaderTemplate):
+class head_1(head_1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
