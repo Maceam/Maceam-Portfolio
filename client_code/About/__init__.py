ad9e8b245dc012dfbb44b58d7da766579cc68244
info@maceam.com <info@maceam.com>
2023-07-22 00:29:40 +0000

    Edited About

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -6,15 +6,9 @@ from anvil.tables import app_tables
 import anvil.server
 import anvil.image
 
-
 class About(AboutTemplate):
-    def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-          self.init_components(**properties)
-          machines = anvil.server.call('get_all_machines')
-          self.apply_view(machines)
-    def about_image(self, view):
-      filerow = app_tables.personal_brand.get(name=logoglitchyy)
-      logoglitchy = Image(source= filerow)
-      printf (filerow)
-    # Any code you write here will run before the form opens.
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.logoglitchy = app_tables.personal_brand.get(q.like(name))
+    self.repeating_panel_1.items=app_tables.table_0.search()
