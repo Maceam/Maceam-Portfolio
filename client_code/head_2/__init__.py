055b8444a589a21b5907eb50590df470b36c082b
info@maceam.com <info@maceam.com>
2023-07-22 06:03:21 +0000

    Created custom package dependencies; Edited settings, About and head_2

--- client_code/head_2/__init__.py
+++ client_code/head_2/__init__.py
@@ -10,25 +10,19 @@ class head_2(head_2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-  def home_click(self, **event_args):
+
+  def Portfolio_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
 
-  def portfolio_click(self, **event_args):
+  def About_click(self, **event_args):
     """This method is called when the link is clicked"""
-    pass
 
-  def about_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def contact_click(self, **event_args):
-    """This method is called when the link is clicked"""
     pass
 
   def Main_click(self, **event_args):
     """This method is called when the link is clicked"""
-
+  pass
   def Contact_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
