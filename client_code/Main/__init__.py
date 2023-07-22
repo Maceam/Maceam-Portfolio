359d6f73f53c779e08508140138ada74a7845801
info@maceam.com <info@maceam.com>
2023-07-22 09:23:30 +0000

    Edited Main, theme.css and theme/parameters.yaml; Deleted head_2

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -8,7 +8,41 @@ from ..Portfolio import Portfolio
 from ..About import About
 from ..Contact import Contact
 
+about_panel = About()
+portfolio_panel = Portfolio()
+contact_panel = Contact()
+
+
+
 class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+  def header_main_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    get_open_form().content_panel.clear()
+    open_form('Main')
+    self.header_main.underline = true
+  def header_portfolio_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    get_open_form().content_panel.clear()
+    get_open_form().content_panel.add_component(portfolio_panel)
+
+    
+  def header_about_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    get_open_form().content_panel.clear()
+    get_open_form().content_panel.add_component(about_panel)
+
+  def header_contact_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    get_open_form().content_panel.clear()
+    get_open_form().content_panel.add_component(contact_panel)
+    self.refresh_data_bindings()
+    
+
+
+
+
+
