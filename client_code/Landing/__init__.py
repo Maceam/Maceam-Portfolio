52e6710333f180d467ded03925bc1d5203ff8360
info@maceam.com <info@maceam.com>
2023-07-27 23:18:56 +0000

    Edited Landing

--- client_code/Landing/__init__.py
+++ client_code/Landing/__init__.py
@@ -9,17 +9,14 @@ class Landing(LandingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     table_row= app_tables.showcase.get(Name=q.like('LandingGirl'))
-    LandImg = Image(display_mode= "zoom_to_fill", source= table_row['PortImg'], vertical_align= "center")
-
-
+    LandImg = Image(display_mode= "fill_width", source= table_row['PortImg'], vertical_align= "center")
+    LandPanel = ColumnPanel()
+    
+    self.init_components(**properties)
+    self.add_component(LandPanel)
+    LandPanel.add_component(LandImg, full_width_row=True)
     #self.add_component(LandImg)
 
-    self.init_components(**properties)
-    self.add_component(LandImg)
-    #table_row= app_tables.showcase.get(Name=q.like('LandingGirl'))
-    #self.LandImg.source = table_row['PortImg']
 
-  #def LandImg_show(self, **event_args):
-    #pass
   # Any code you write here will run before the form opens.
-  #def Landing(self, **event_args):
+
