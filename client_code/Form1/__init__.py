442cb456d3d331a64f31ea1b89a5ca0e8b90e8fa
info@maceam.com <info@maceam.com>
2023-07-27 05:44:53 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -22,21 +22,19 @@ class Form1(Form1Template):
   def canvas_1_reset(self, **event_args):
     """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
     c = self.canvas_1
-    self.canvas_1.clear_rect(0, 0, self.canvas_1.width, self.canvas_1.height)
-    c.begin_path()
-    c.move_to(100,100)
-    c.line_to(100,200)
-    c.line_to(self.canvas_1.width,self.canvas_1.height)
-    c.close_path()
-  
+        # Set the stroke and fill styles
     c.stroke_style = "#2196F3"
     c.line_width = 3
     c.fill_style = "#E0E0E0"
   
-    c.fill()
-    c.stroke()
-
-
+    # Draw a filled rectangle
+    c.fill_rect(100, 100, 50, 75)
+  
+    # Draw a rectangle outline 25 pixels right of it
+    c.stroke_rect(125, 100, 50, 75)
+  
+    # Remove colour from a smaller rectangle
+    c.clear_rect(120, 125, 20, 30)
 
 
     
