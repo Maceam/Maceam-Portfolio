bede1e20105f5240ce1b513cf75f2a06fa8c35d8
info@maceam.com <info@maceam.com>
2023-07-28 02:51:17 +0000

    Edited Landing

--- client_code/Landing/__init__.py
+++ client_code/Landing/__init__.py
@@ -8,19 +8,16 @@ from anvil.tables import app_tables
 class Landing(LandingTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    
     # Set Form properties and Data Bindings.
+    self.LandginMethod()
+  
+
+  #make a def (aka method) for adding the panel  
+  def LandginMethod(self, **event_args): 
+  # Any code you write here will run before the form opens.
     table_row= app_tables.showcase.get(Name=q.like('LandingGirl'))
     LandImg = Image(display_mode= "fill_width", source= table_row['PortImg'], vertical_align= "center")
     LandPanel = ColumnPanel()
-    
-    
     self.add_component(LandPanel)
     LandPanel.add_component(LandImg, full_width_row=True)
     LandImg.role="LandingImg"
-    #self.add_component(LandImg)
-
-  #make a def (aka method) for adding the panel  
-
-  # Any code you write here will run before the form opens.
-
