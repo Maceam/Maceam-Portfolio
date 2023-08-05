ff315107ce26fde77a42123ea324802f6cfaa7ce
info@maceam.com <info@maceam.com>
2023-08-05 08:05:12 +0000

    Edited Portfolio.BuildersCradle

--- client_code/Portfolio/BuildersCradle/__init__.py
+++ client_code/Portfolio/BuildersCradle/__init__.py
@@ -13,20 +13,16 @@ class BuildersCradle(BuildersCradleTemplate):
     self.RepeatC2C()
     
   def RepeatC2C(self, **event_args):
-    HeroPanel= ColumnPanel()
+    HeroPanel= FlowPanel()
     C2CFlowPanel= FlowPanel()
     spacertest= Spacer()
     self.add_component(HeroPanel)
     self.add_component(spacertest)
     self.add_component(C2CFlowPanel)
-    HeroWork= app_tables.showcase.search(Name=q.like('C2CHero%'))
-    for Name in HeroWork:
-      HeroImg= Image(display_mode= 'full_width', source= Name['PortImg'], vertical_align= "center", role= "faded")
-      HeroPanel.add_component(HeroImg)
-      
+
     FolioWork= app_tables.showcase.search(Name=q.like('C2C%'))
     for Name in FolioWork:
-      img = Image(source= Name['PortImg'], vertical_align= "center", role= "faded", height= 400)
+      img = Image(source= Name['PortImg'], vertical_align= "center", role= "faded")
       C2CFlowPanel.add_component(img)
 
     
