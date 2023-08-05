c645e4da6d63cad836bd508105ddd6fc3c436aac
info@maceam.com <info@maceam.com>
2023-08-05 07:28:13 +0000

    Edited Main and Portfolio.BuildersCradle

--- client_code/Portfolio/BuildersCradle/__init__.py
+++ client_code/Portfolio/BuildersCradle/__init__.py
@@ -13,14 +13,21 @@ class BuildersCradle(BuildersCradleTemplate):
     self.RepeatC2C()
     
   def RepeatC2C(self, **event_args):
+    HeroPanel= ColumnPanel()
     C2CFlowPanel= FlowPanel()
+    spacertest= Spacer()
+    self.add_component(HeroPanel)
+    self.add_component(spacertest)
     self.add_component(C2CFlowPanel)
-    #table_row_total= len(app_tables.showcase.search()) #not nessesary just tells the total table size
+    HeroWork= app_tables.showcase.search(Name=q.like('C2CHero%'))
+    for Name in HeroWork:
+      HeroImg= Image(display_mode= 'full_width', source= Name['PortImg'], vertical_align= "center", role= "faded")
+      HeroPanel.add_component(HeroImg)
+      
     FolioWork= app_tables.showcase.search(Name=q.like('C2C%'))
     for Name in FolioWork:
-      img = Image(display_mode= "fill_width", source= Name['PortImg'], vertical_align= "center", role= "faded")
+      img = Image(source= Name['PortImg'], vertical_align= "center", role= "faded", height= 400)
       C2CFlowPanel.add_component(img)
-      
 
     
     
