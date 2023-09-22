ccdeb1f8fbbed378dee68abc98719dc2067eac7b
info@maceam.com <info@maceam.com>
2023-09-22 11:13:59 +0000

    Created README.md; Edited Portfolio

--- client_code/Portfolio/__init__.py
+++ client_code/Portfolio/__init__.py
@@ -10,15 +10,22 @@ from ..Portfolio.BuildersCradle import BuildersCradle
 from ..Portfolio.D0tmatrix import D0tmatrix
 from ..Portfolio.Onlied import Onlied
 
+def PanelReset(self, panel_name):
+  ReAdd= Portfolio() 
+  get_open_form().content_panel.clear()
+  get_open_form().content_panel.add_component(panel_name)
+  get_open_form().content_panel.add_component(ReAdd)
+    
 class Portfolio(PortfolioTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
     self.ButtonHolder()
-    
+
+
   def C2CButton(self, **event_args):
-    ButtonExp= Button(text= "The Builders Cradel", align= "full", font_size= 22, spacing_above= "none", spacing_below= "none")
+    ButtonExp= Button(text= "The Builder's Cradle", align= "full", font_size= 22, spacing_above= "none", spacing_below= "none")
     ButtonExp.role= "ButtonPortfolio"
     self.add_component(ButtonExp)
     ButtonExp.add_event_handler('click', self.C2CButton_click)
@@ -26,9 +33,7 @@ class Portfolio(PortfolioTemplate):
   def C2CButton_click(self, **event_args):
     ReAdd= Portfolio() 
     BuildersCradleForm= BuildersCradle()
-    get_open_form().content_panel.clear()
-    get_open_form().content_panel.add_component(BuildersCradleForm)
-    get_open_form().content_panel.add_component(ReAdd)
+    PanelReset(self, BuildersCradleForm)
 
   def D0tmatrixButton(self, **event_args):
     ButtonExp= Button(text= "D0tmatrix: Typeface", align= "full", font_size= 22, spacing_above= "small", spacing_below= "small")
